972639d3e7793194f2cb5c13e8a0d4da667ff827
ab.dev <ab.dev@MacBook-Air-Aleksandr.local>
2020-12-18 12:55:21 +0300

    test 2

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "alerts-and-pickers",
+    name: "Alerts&Pickers",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "alerts-and-pickers",
-            targets: ["alerts-and-pickers"]),
+            name: "Alerts&Pickers",
+            targets: ["Alerts&Pickers"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "alerts-and-pickers",
+            name: "Alerts&Pickers",
             dependencies: []),
         .testTarget(
-            name: "alerts-and-pickersTests",
-            dependencies: ["alerts-and-pickers"]),
+            name: "Alerts&Pickers",
+            dependencies: ["Alerts&Pickers"]),
     ]
 )
