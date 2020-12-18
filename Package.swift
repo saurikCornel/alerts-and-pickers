6b67d2d894a6a946d6a602ff88cb03c51afe0afa
ab.dev <ab.dev@MacBook-Air-Aleksandr.local>
2020-12-18 13:24:55 +0300

    Final

--- Package.swift
+++ Package.swift
@@ -1,24 +1,20 @@
-// swift-tools-version:5.1
-
 import PackageDescription
 
-let package = Package(
-    name: "AlertsPickers",
-    platforms: [
-        .iOS(.v12),
-    ],
-    products: [
-        .library(name: "AlertsPickers", targets: ["AlertsPickers"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "AlertsPickers",
-            dependencies: [],
-            path: ".",
-            sources: ["AlertsPickers/"],
-            publicHeadersPath: "AlertsPickers"
-        )
-    ]
-)
-
+ let package = Package(
+     name: "Alerts&Pickers",
+     products: [
+         // Products define the executables and libraries a package produces, and make them visible to other packages.
+         .library(
+             name: "Alerts&Pickers",
+             targets: ["Alerts&Pickers"]),
+     ],
+     dependencies: [
+         // Dependencies declare other packages that this package depends on.
+ let package = Package(
+         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+         .target(
+             name: "Alerts&Pickers",
+             dependencies: []),
+     ]
+ )
