155666e245e83af3a60807865e3fd5e6560dfa20
ab.dev <ab.dev@MacBook-Air-Aleksandr.local>
2020-12-18 13:18:47 +0300

    test 3

--- Package.swift
+++ Package.swift
@@ -1,28 +1,24 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
-    name: "Alerts&Pickers",
+    name: "AlertsPickers",
+    platforms: [
+        .iOS(.v12),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Alerts&Pickers",
-            targets: ["Alerts&Pickers"]),
+        .library(name: "AlertsPickers", targets: ["AlertsPickers"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Alerts&Pickers",
-            dependencies: []),
-        .testTarget(
-            name: "Alerts&Pickers",
-            dependencies: ["Alerts&Pickers"]),
+            name: "AlertsPickers",
+            dependencies: [],
+            path: ".",
+            sources: ["AlertsPickers/"],
+            publicHeadersPath: "AlertsPickers"
+        )
     ]
 )
+
